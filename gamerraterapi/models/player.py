730404cfcf01ff2af13d4abb0efaf82f7e812493
emilycentko <emily.centko@gmail.com>
2021-05-10 10:13:47 -0500

    Seeded database

--- gamerraterapi/models/player.py
+++ gamerraterapi/models/player.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Player(models.Model):
 
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=80)
